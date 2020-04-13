792549e981f5370f07502cab8ca4063e3c76feb9
Jonas Gunz <himself@jonasgunz.de>
2020-04-13 11:31:50 +0200

    Initial

---
+++ src/main/java/re/jag/parquet/Parquet.java
@@ -0,0 +1,13 @@
+package re.jag.parquet;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Parquet implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/re/jag/parquet/mixin/VanillaMixin.java
@@ -0,0 +1,16 @@
+package re.jag.parquet.mixin;
+
+import net.minecraft.server.MinecraftServer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftServer.class)
+public class VanillaMixin {
+	@Inject(method = "getServerModName", at = @At("RETURN"), cancellable = true)
+	protected void onGetServerModName(CallbackInfoReturnable<String> cir) {
+		cir.setReturnValue("vanilla+™");
+	}
+}
